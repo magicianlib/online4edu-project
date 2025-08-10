5f5f3c0369edecf3bb65f1783273e0e604ea29e9
magicianlib <magicianlib@gmail.com>
2025-08-10 15:03:18 +0800

    pref

--- online4edu-dependencies/online4edu-dependencies-utils/src/main/java/com/online4edu/dependencies/utils/bitwise/IntMask.java
+++ online4edu-dependencies/online4edu-dependencies-utils/src/main/java/com/online4edu/dependencies/utils/bitwise/IntMask.java
@@ -1,5 +1,7 @@
 package com.online4edu.dependencies.utils.bitwise;
 
+import java.util.List;
+
 /**
  * int 二进制掩码操作
  * <pre>
@@ -13,7 +15,7 @@ package com.online4edu.dependencies.utils.bitwise;
  *
  *     @Override
  *     public int index() {
- *         return this.ordinal();
+ *         return this.ordinal() + 1;
  *     }
  * }
  * }
@@ -23,24 +25,62 @@ package com.online4edu.dependencies.utils.bitwise;
  * @see LongMask
  */
 public interface IntMask {
+    /**
+     * 索引小于或等于0，无效（不设置 mask）
+     */
     int index();
 
+    /**
+     * 清除 mask
+     */
     default int clean(int bitmask) {
         return set(bitmask, false);
     }
 
+    default int set(int bitmask) {
+        return set(bitmask, true);
+    }
+
+    /**
+     * 设置 mask
+     */
     default int set(int bitmask, boolean value) {
         if (index() <= 0) {
             return bitmask;
         }
+        if (bitmask < 0) {
+            bitmask = 0;
+        }
+
         int mask = 1 << index();
         if (value) {
-            return bitmask |= mask;
+            return bitmask | mask;
         }
-        return bitmask &= ~mask;
+        return bitmask & ~mask;
     }
 
+    /**
+     * 测试是否已设置 mask
+     */
     default boolean test(int bitmask) {
         return (bitmask & (1 << index())) != 0;
+    }
+
+    static int clean(int bitmask, List<IntMask> masks) {
+        for (IntMask mask : masks) {
+            bitmask = mask.clean(bitmask);
+        }
+        return bitmask;
+    }
+
+    static int set(List<IntMask> masks) {
+        return set(0, masks);
+    }
+
+    static int set(int bitmask, List<IntMask> masks) {
+        for (IntMask mask : masks) {
+            bitmask = mask.set(bitmask);
+        }
+        return bitmask;
     }
 }
--- online4edu-dependencies/online4edu-dependencies-utils/src/main/java/com/online4edu/dependencies/utils/bitwise/LongMask.java
+++ online4edu-dependencies/online4edu-dependencies-utils/src/main/java/com/online4edu/dependencies/utils/bitwise/LongMask.java
@@ -1,5 +1,7 @@
 package com.online4edu.dependencies.utils.bitwise;
 
+import java.util.List;
+
 /**
  * long 二进制掩码操作
  * <pre>
@@ -13,7 +15,7 @@ package com.online4edu.dependencies.utils.bitwise;
  *
  *     @Override
  *     public long index() {
- *         return this.ordinal();
+ *         return this.ordinal() + 1;
  *     }
  * }
  * }
@@ -23,16 +25,33 @@ package com.online4edu.dependencies.utils.bitwise;
  * @see IntMask
  */
 public interface LongMask {
+    /**
+     * 索引小于或等于0，无效（不设置 mask）
+     */
     long index();
 
+    /**
+     * 清除 mask
+     */
     default long clean(long bitmask) {
         return set(bitmask, false);
     }
 
+    default long set(long bitmask) {
+        return set(bitmask, true);
+    }
+
+    /**
+     * 设置 mask
+     */
     default long set(long bitmask, boolean value) {
         if (index() <= 0L) {
             return bitmask;
         }
+        if (bitmask < 0L) {
+            bitmask = 0L;
+        }
+
         long mask = 1L << index();
         if (value) {
             return bitmask | mask;
@@ -40,7 +59,28 @@ public interface LongMask {
         return bitmask & ~mask;
     }
 
+    /**
+     * 测试是否已设置 mask
+     */
     default boolean test(long bitmask) {
         return (bitmask & (1L << index())) != 0L;
+    }
+
+    static long clean(long bitmask, List<LongMask> masks) {
+        for (LongMask mask : masks) {
+            bitmask = mask.clean(bitmask);
+        }
+        return bitmask;
+    }
+
+    static long set(List<LongMask> masks) {
+        return set(0L, masks);
+    }
+
+    static long set(long bitmask, List<LongMask> masks) {
+        for (LongMask mask : masks) {
+            bitmask = mask.set(bitmask);
+        }
+        return bitmask;
     }
 }
